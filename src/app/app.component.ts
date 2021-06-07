29bd23a14d0a8c3677ed26e567eb806ff7b1e296
saiVenkat <bhaskarpotala@gmail.com>
2021-06-07 16:52:06 +0530

    angular 1st class

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,10 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
+
+
 export class AppComponent {
   title = 'demo';
+  url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
 }
