f7dcfa8eb39c4adf916ab589db146db1626f2a82
saiVenkat <saipotala397@gmail.com>
2021-06-08 15:02:01 +0530

    login-logout-task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,66 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-
-
-
 export class AppComponent {
-  title = 'demo';
-  url = "https://www.google.com/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+  ngOnInit(): void {
+    let user: object;
+    let userArray: Array<any> = [];
+    let counter : number = 0;
+    let form = document.getElementById('form-register') as HTMLFormElement;
+
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let email = document.getElementById('email') as HTMLInputElement;
+      let password = document.getElementById('password') as HTMLInputElement;
+      let name = document.getElementById('name') as HTMLInputElement;
+      user = {
+        name: name.value,
+        email: email.value,
+        password: password.value,
+      };
+      userArray.push(user);
+      console.log(userArray);
+    });
+
+
+    let formLogin = document.getElementById('form-login') as HTMLFormElement;
+
+    formLogin.addEventListener('submit', (e) => {
+      e.preventDefault();
+      let email = document.getElementById('email-login') as HTMLInputElement;
+      let password = document.getElementById(
+        'password-login'
+      ) as HTMLInputElement;
+
+      userArray.forEach((element,idx) =>{
+        if(element.email === email.value){
+          if (element.password === password.value) {
+               counter += 1 
+               alert("logged in")
+              
+              
+          }
+          else{
+            alert("wrong password")
+            return;
+          }
+        }
+
+        if (counter === 0) {
+          alert('invalid credentials');
+        }
+
+      })
+
+      
+
+
+      
+    });
+  }
 }
